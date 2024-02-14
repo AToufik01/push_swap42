de98397c2b3f8d05a4eddede28a0680ac305c484
AToufik01 <128374966+AToufik01@users.noreply.github.com>
2024-02-14 18:39:00 +0100

    update r_r

--- push_swap/rules/reverse_rotate.c
+++ push_swap/rules/reverse_rotate.c
@@ -1,11 +1,30 @@
 #include "push_swap.h"
 
-void    reverse_rotate(t_stack **a)
+void    reverse_rotate(t_stack **head)
 {
-
+    t_stack *last;
+    last = *head;
+    while (last->next->next)
+        last = last->next;
+    last->next->next = *head;
+    last->next = NULL;
 }
 
+void	rra(t_stack **a)
+{
+    reverse_rotate(a);
+    ft_putendl_fd("rra", 1);
+}
 
-void	rra(t_stack **a);
-void	rrb(t_stack **b);
-void	rrr(t_stack **a, t_stack **b);
+void	rrb(t_stack **b)
+{
+    reverse_rotate(b);
+    ft_putendl_fd("rrb", 1);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+        reverse_rotate(a);
+        reverse_rotate(b);
+        ft_putendl_fd("rrr", 1);
+}
