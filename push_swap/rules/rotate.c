8d87dfaebb313fd83e85b5fc048d4f458d6bc359
AToufik01 <128374966+AToufik01@users.noreply.github.com>
2024-02-14 18:23:21 +0100

    update

--- push_swap/rules/rotate.c
+++ push_swap/rules/rotate.c
@@ -1,9 +1,34 @@
 #include "push_swap.h"
 
-void    rotate(t_stack **a)
+void    rotate(t_stack **head)
 {
+    t_stack *curr;
+    t_stack *node;
 
+    curr = *head;
+    node = curr;
+    while (curr)
+        curr = curr->next;
+    (*head) = (*head)->next;
+    curr->next = node;
+    node->next = NULL;
 }
-void	ra(t_stack **a);
-void	rb(t_stack **b);
-void	rr(t_stack **a, t_stack **b);
+
+void	ra(t_stack **a)
+{
+    rotate(a);
+    ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_stack **b)
+{
+    rotate(b);
+    ft_putendl_fd("rb", 1);
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+    rotate(a);
+    rotate(b);
+    ft_putendl_fd("rr", 1);
+}
